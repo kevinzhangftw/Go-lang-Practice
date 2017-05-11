cb65b475c710dfdde78ba66da0ace39f9a2fe8a1
Kevin Zhang <kevin.c.t.zhang@gmail.com>
2017-05-11 14:01:43 -0700

    adding headers for time file and tests

---
+++ time.go
@@ -0,0 +1,22 @@
+package a1
+	
+import (
+	// "bufio"
+    // "fmt"
+    // "io/ioutil"
+    // "os"
+)
+
+type Time24 struct {
+    hour, minute, second uint8
+}
+// 0 <= hour < 24
+// 0 <= minute < 60
+// 0 <= second < 60
+
+func equalsTime24(a Time24, b Time24) bool {
+    equal := false
+    // if a equal b return true
+
+    return equal
+}
---
+++ time_test.go
@@ -0,0 +1,15 @@
+package a1
+	
+import (
+	"testing"
+)
+
+type Time24 struct {
+    hour, minute, second uint8
+}
+
+func TestEqualsTime24(t *testing.T) {
+
+
+
+}
