b5eea0c2f4bece9fda21abf88d7783b6a9b68f8d
Kevin Zhang <kevin.c.t.zhang@gmail.com>
2017-05-11 22:50:39 -0700

    basic test for time

--- time_test.go
+++ time_test.go
@@ -4,12 +4,16 @@ import (
 	"testing"
 )
 
-type Time24 struct {
-    hour, minute, second uint8
-}
-
 func TestEqualsTime24(t *testing.T) {
-
-
+	a:= Time24{
+		9,30,30,
+	}
+	b:= Time24{
+		9,30,30,
+	}
+	abTimeIsEqual := equalsTime24(a,b)
+	if !abTimeIsEqual {
+		t.Errorf("time for a and b should be equal, but it is not.")
+	}
 
 }
