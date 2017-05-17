7eec6877193f0dcd88943c56228a49ecf80c2a7e
Kevin Zhang <kevin.c.t.zhang@gmail.com>
2017-05-17 15:54:18 -0700

    lin search test failed

--- LinearSearch.go
+++ LinearSearch.go
@@ -7,12 +7,38 @@ import (
     // "os"
 	// "strconv"
 	// "errors"
+	"reflect"
 )
 
-func linearSearch(x interface{}, lst interface{}) int {
-	fmt.Println(x)
-	fmt.Println(lst)
-	index := 0
-	
-	return index
+//adapted from http://stackoverflow.com/questions/12753805/type-converting-slices-of-interfaces-in-go
+func linearSearch(x interface{}, lst interface{}) (index int) {
+	// xVal := reflect.ValueOf(x)
+	// fmt.Println(xVal)
+
+	// lstVal := reflect.ValueOf(lst)
+	// if lstVal.Kind() != reflect.Slice {
+ //        panic("lst is not a slice")
+ //    }
+ //    lstInterface := make([]interface{}, lstVal.Len())
+ //    for i := 0; i < lstVal.Len(); i++ {
+ //    	lstInterface[i] = lstVal.Index(i).Interface    	
+ //    }
+ //    fmt.Println(lstInterface)
+	uselessInt:= 5
+	uselessString:="i am useless"
+	if reflect.TypeOf(x) == uselessInt.(int) {
+		fmt.Println("x is int")	
+	}else if reflect.TypeOf(x) == uselessString.(string){
+		fmt.Println("x is string")
+	}
+
+
+    //check if x and lst[] is same type
+
+
+
+    
+
+
+	return
 }
--- LinearSearch_test.go
+++ LinearSearch_test.go
@@ -2,12 +2,11 @@ package a1
 	
 import (
 	"testing"
-	"fmt"
+	// "fmt"
 )
 
-func testLinearSearch(t *testing.T) {
-	test1 := linearSearch(5, []int{4, 2, -1, 5, 0})
-	fmt.Println(test1)
+func TestLinearSearch(t *testing.T) {
+	test1 := linearSearch(5, []int{4, 2, -1, 5, 0})	
 	if test1 != 3 {
 		t.Errorf("test1 should return 3, instead it returns %v", test1)
 	}
